da63646bf317b07bde2517104e70390f486e2c8b
Christopher Hullman <christopherhullman@gmail.com>
2022-10-25 13:55:58 +1000

    wrote most of the functionality for the user router-level middleware. Few 'to do' comments added for points to come back to

---
+++ keys/firebaseClientKey.js
@@ -0,0 +1,12 @@
+// Firebase client-side API configuration
+const firebaseClientConfig = {
+    apiKey: "AIzaSyD5IPJxv0EMB7ZNm0qShwmqndVeDSpdzZU",
+    authDomain: "co-cleanup.firebaseapp.com",
+    projectId: "co-cleanup",
+    storageBucket: "co-cleanup.appspot.com",
+    messagingSenderId: "742262156806",
+    appId: "1:742262156806:web:75e0cea87bf53320aa9d87",
+    measurementId: "G-0DVVKVVZQJ"
+  }
+
+  module.exports = { firebaseClientConfig };
---
+++ src/User/UserRoutes.js
@@ -0,0 +1,112 @@
+// This code has been modified from Alex Holder's MERN Masterclass presented to Coder Academy on the 17th October, 2022
+// Link Alex's Masterclass tutorial is: https://github.com/AlexHolderDeveloper/expressjs-class-oct-22
+const express = require("express");
+
+// The Express router provides router-level middleware so that we can
+// define middleware specifically for all routes from the /user URL in this case
+// Keeps things tidy by keeping router level functionality outside of ../index.js
+const routes = express.Router();
+
+// functions imported from ./UserFunctions.js
+const {
+  signUpUser,
+  signInUser,
+  validateUserSession,
+} = require("./UserFunctions");
+
+// Create a user, a session token & a refresh token
+routes.post("/sign-up", async (request, response) => {
+    // request data formatted ready for input into signUpUser function
+  let newUserDetails = {
+    email: request.body.email,
+    password: request.body.password,
+    displayName: request.body.username,
+  };
+  // To Do
+  // Ideally perform validation on those properties before moving on.
+  // Not in the scope of this guide though! ;)
+
+  // Hand newUserDetails data to a signUpUser function
+  let signUpResult = await signUpUser({
+    email: newUserDetails.email,
+    password: newUserDetails.password,
+  });
+  // Return error or token as response
+  if (signUpResult.error != null) {
+    console.log(
+      "Stopping the signup process due to an error. See logs for details."
+    );
+    response.json(signUpResult);
+    return;
+  }
+
+  // Sign in to get latest user claims (authorization).
+  let signInResult = await signInUser({
+    email: newUserDetails.email,
+    password: newUserDetails.password,
+  });
+
+  // If an error message exists, return that.
+  if (signInResult.error != null) {
+    console.log(
+      "Stopping the signup process due to an error. See logs for details."
+    );
+    response.json(signInResult);
+    return;
+  }
+
+  // On success, return a signed-in session to the brand-new user:
+  response.json(signInResult);
+});
+
+// Create a session token & refresh token
+routes.post("/sign-in", async (request, response) => {
+  // Process posted form/json data
+  let userDetails = {
+    email: request.body.email,
+    password: request.body.password,
+    displayName: request.body.username,
+  };
+
+  // To Do
+  // Ideally perform validation on those properties before moving on.
+  // Not in the scope of this guide though! ;)
+
+  // Hand data to a sign-in function
+  let signInResult = await signInUser({
+    email: userDetails.email,
+    password: userDetails.password,
+  });
+
+  // If an error message exists, return that.
+  if (signInResult.error != null) {
+    console.log(
+      "Stopping the signup process due to an error. See logs for details."
+    );
+    response.json(signInResult);
+    return;
+  }
+
+  // On success, return a signed-in session to the brand-new user:
+  response.json(signInResult);
+});
+
+// Create a session token & refresh token
+routes.post("/validate-session", async (request, response) => {
+  // Process posted form/json data
+  let sessionDetails = {
+    idToken: request.body.idToken,
+    refreshToken: request.body.refreshToken,
+  };
+
+  // Hand data to a validation function
+  let validationResult = await validateUserSession({
+    refreshToken: sessionDetails.refreshToken,
+    idToken: sessionDetails.idToken,
+  });
+
+  // Return error or token as response
+  response.json(validationResult);
+});
+
+module.exports = routes;
